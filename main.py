bc8fdbae7e60d4d90d7ec0c69be73bbee14ba407
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-09-26 01:26:07 -0300

    implement getitem and len in Pile class. turn "calc_possible_states" procedural for any number of piles.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
 @dataclass
 class Pile:
-    pile: List[int]
     max_size: int
     name: str
+    _pile: List[int] = field(default_factory=list)
+    pile_width: int = 7
+    disk_width: int = 1
 
     def __str__(self):
-        pile_string_lines = [self.disk_to_str(disk) for disk in self.pile]
-        for i in range(len(self.pile), self.max_size):
+        pile_string_lines = [self.disk_to_str(disk) for disk in self._pile]
+        for i in range(len(self._pile), self.max_size):
             pile_string_lines = ["|", *pile_string_lines]
-        w = 7
+        w = self.pile_width
         pile_string_lines = list(map(lambda x: x.center(w, " "), pile_string_lines))
         pile_string_lines.append("|".center(w, "_"))
         pile_string_lines.append(self.name.center(w))
@@ -21,13 +22,23 @@ class Pile:
 
     def __hash__(self):
         m = str_to_int(self.name)
-        for disk in self.pile:
+        for disk in self._pile:
             m *= (disk + 1)
         return m
 
+    def __getitem__(self, item):
+        return self._pile[item]
+
+    def __len__(self):
+        return len(self._pile)
+
+    def reversed(self):
+        _pile_copy = self._pile.copy()
+        _pile_copy.reverse()
+        return _pile_copy
+
     def disk_to_str(self, disk_size):
-        w = 1
-        middle = ["="] * (disk_size * w * 2 - 1)
+        middle = ["="] * (disk_size * self.disk_width * 2 - 1)
         disk_str = f"<{''.join(middle)}>"
         return disk_str
 
@@ -53,7 +64,8 @@ class State:
         st_str = ""
         piles_as_lists_of_strings = [str(pile).split("\n") for pile in self.piles]
         for line_number in range(len(piles_as_lists_of_strings[0])):
-            st_str += "  ".join([piles_as_lists_of_strings[pile_number][line_number] for pile_number in range(len(self.piles))])
+            st_str += "  ".join(
+                [piles_as_lists_of_strings[pile_number][line_number] for pile_number in range(len(self.piles))])
             st_str += "\n"
         if DEBUG:
             st_str += f"{self.move_str}   {self.h()}   {self.__hash__()}"
@@ -78,88 +90,70 @@ def calc_h(state):
     0. Start with 0
     1. Let fault mean a disk placed on top of another one which is
         not its direct successor, add 1 for each fault in all piles.
-    2. Add 1.5 for each disk that is not on the target pile.
-    3. Add 1 for each disk in the target pile that is not on its
+    2. Add 1.5 for each disk that is not on the target _pile.
+    3. Add 1 for each disk in the target _pile that is not on its
         correct final place, as it would be when the game is over.
     """
-    p1, p2, p3 = map(lambda x: x.pile, state.piles)
-    n_disks = len(p1 + p2 + p3)
+    p1, p2, p3 = state.piles
+    n_disks = len(p1) + len(p2) + len(p3)
     h = 0
 
     for pile in (p1, p2, p3):
         h += faults(pile)
 
-    h += 1.5 * len(p1 + p2)
+    h += 1.5 * (len(p1) + len(p2))
 
     h += errors(p3, n_disks)
     return h
 
 
 def faults(pile):
-    f = 0
+    fault_count = 0
     for i in range(len(pile) - 1):
         if pile[i + 1] - pile[i] > 1:
-            f += 1
-    return f
+            fault_count += 1
+    return fault_count
 
 
-def errors(pile, n_disks):
-    # since piles are bound to objects, changing them changes the parent object
-    # so we must make a copy first (not a reference)
-    pile_copy = deepcopy(pile)
-    pile_copy.reverse()
-    e = 0
-    for i in range(len(pile_copy)):
-        if pile_copy[i] != n_disks:
-            e += 1
+def errors(pile: Pile, n_disks):
+    pile_reversed = pile.reversed()
+    error_count = 0
+    for i in range(len(pile_reversed)):
+        if pile_reversed[i] != n_disks:
+            error_count += 1
         n_disks -= 1
-    return e
+    return error_count
 
 
 def calc_possible_states(state):
     possible_states = []
-    P1, P2, P3 = state.piles
-    p1, p2, p3 = map(lambda x: x.pile, (P1, P2, P3))
-    if len(p1) > 0:
-        if can_move(P1, P2):
-            P1_, P2_ = move_one(P1, P2)
-            possible_states.append(State([P1_, P2_, P3], f"{P1_.name}->{P2_.name}"))
-        if can_move(P1, P3):
-            P1_, P3_ = move_one(P1, P3)
-            possible_states.append(State([P1_, P2, P3_], f"{P1_.name}->{P3_.name}"))
-    if len(p2) > 0:
-        if can_move(P2, P1):
-            P2_, P1_ = move_one(P2, P1)
-            possible_states.append(State([P1_, P2_, P3], f"{P2_.name}->{P1_.name}"))
-        if can_move(P2, P3):
-            P2_, P3_ = move_one(P2, P3)
-            possible_states.append(State([P1, P2_, P3_], f"{P2_.name}->{P3_.name}"))
-    if len(p3) > 0:
-        if can_move(P3, P1):
-            P3_, P1_ = move_one(P3, P1)
-            possible_states.append(State([P1_, P2, P3_], f"{P3_.name}->{P1_.name}"))
-        if can_move(P3, P2):
-            P3_, P2_ = move_one(P3, P2)
-            possible_states.append(State([P1, P2_, P3_], f"{P3_.name}->{P2_.name}"))
+    for pile_from in state.piles:
+        for pile_to in state.piles:
+            if pile_to == pile_from:
+                continue
+            new_piles = state.piles.copy()
+            if can_move(pile_from, pile_to):
+                new_pile_from, new_pile_to = move_one(pile_from, pile_to)
+                new_piles[state.piles.index(pile_from)] = new_pile_from
+                new_piles[state.piles.index(pile_to)] = new_pile_to
+                possible_states.append(State(new_piles, f"{new_pile_from.name}->{new_pile_to.name}"))
     return possible_states
 
 
 def can_move(pile1, pile2):
     """ From pile1 to pile2 """
-    if len(pile1.pile) == 0:
+    if len(pile1) == 0:
         return False
-    if len(pile2.pile) == 0:
+    if len(pile2) == 0:
         return True
-    if pile1.pile[0] < pile2.pile[0]:
+    if pile1[0] < pile2[0]:
         return True
 
 
 def move_one(pile1, pile2):
     """ From pile1 to pile2 """
-    p1 = deepcopy(pile1)
-    p2 = deepcopy(pile2)
-    p2.pile = [p1.pile[0], *p2.pile]
-    p1.pile = p1.pile[1:]
+    p2 = Pile(_pile=[pile1[0], *pile2[:]], name=pile2.name, max_size=pile2.max_size)
+    p1 = Pile(_pile=pile1[1:], name=pile1.name, max_size=pile1.max_size)
     return p1, p2
 
 
@@ -180,8 +174,8 @@ def move(state, visited_states):
 
 
 def hanoi(n):
-    start = Pile(pile=[i for i in range(1, n + 1)], name="start", max_size=n)
-    aux, end = Pile(pile=[], name="aux", max_size=n), Pile(pile=[], name="end", max_size=n)
+    start = Pile(_pile=[i for i in range(1, n + 1)], name="start", max_size=n)
+    aux, end = Pile(name="aux", max_size=n), Pile(name="end", max_size=n)
     state = State([start, aux, end], "")
     print("INITIAL STATE:")
     print(state)
@@ -197,8 +191,8 @@ def hanoi(n):
 
 if __name__ == "__main__":
     DEBUG = False
-    n = 3
-    moves = hanoi(n)
-    print(moves)
-    print(f"Total moves: {len(moves)}")
-    print(f"Minimum moves: {2 ** n - 1}")
+    number_of_disks = 4
+    output = hanoi(number_of_disks)
+    print(output)
+    print(f"Total moves: {len(output)}")
+    print(f"Minimum moves: {2 ** number_of_disks - 1}")
